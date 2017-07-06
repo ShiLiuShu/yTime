3ab776550383563c3595b10cf3aa5b8044d3f88e
ShiLiuShu <940338582@qq.com>
2017-07-06 22:25:37 +0800

    second

---
+++ src/pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home-root">
+      <p>this is the {{message}} page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+      return {
+          message:"home"
+      }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './home.scss'
+</style>
